8e42892770e98a0e30d41c2f6e15189b8d341728
Mwesigye John Bosco <johnboscomwesigye@gmail.com>
2023-07-21 11:34:03 +0300

    INKO-71 persist loan account decision state

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanDecisionWritePlatformServiceJpaRepositoryImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/LoanDecisionWritePlatformServiceJpaRepositoryImpl.java
@@ -18,17 +18,30 @@
  */
 package org.apache.fineract.portfolio.loanaccount.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.fineract.infrastructure.configuration.data.GlobalConfigurationPropertyData;
+import org.apache.fineract.infrastructure.configuration.domain.ConfigurationDomainService;
+import org.apache.fineract.infrastructure.configuration.service.ConfigurationReadPlatformService;
 import org.apache.fineract.infrastructure.core.api.JsonCommand;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
 import org.apache.fineract.infrastructure.core.data.CommandProcessingResultBuilder;
+import org.apache.fineract.infrastructure.core.exception.GeneralPlatformDomainRuleException;
 import org.apache.fineract.infrastructure.security.service.PlatformSecurityContext;
-import org.apache.fineract.portfolio.businessevent.domain.loan.LoanApprovedBusinessEvent;
-import org.apache.fineract.portfolio.businessevent.service.BusinessEventNotifierService;
-import org.apache.fineract.portfolio.loanaccount.domain.Loan;
-import org.apache.fineract.portfolio.loanaccount.domain.LoanRepositoryWrapper;
-import org.apache.fineract.portfolio.loanaccount.serialization.LoanApplicationTransitionApiJsonValidator;
+import org.apache.fineract.portfolio.calendar.domain.Calendar;
+import org.apache.fineract.portfolio.calendar.domain.CalendarEntityType;
+import org.apache.fineract.portfolio.calendar.domain.CalendarInstance;
+import org.apache.fineract.portfolio.calendar.domain.CalendarInstanceRepository;
+import org.apache.fineract.portfolio.client.domain.Client;
+import org.apache.fineract.portfolio.client.exception.ClientNotActiveException;
+import org.apache.fineract.portfolio.group.domain.Group;
+import org.apache.fineract.portfolio.group.exception.GroupNotActiveException;
+import org.apache.fineract.portfolio.loanaccount.api.LoanApiConstants;
+import org.apache.fineract.portfolio.loanaccount.domain.*;
+import org.apache.fineract.portfolio.loanaccount.loanschedule.service.LoanScheduleAssembler;
+import org.apache.fineract.portfolio.loanaccount.serialization.LoanDecisionTransitionApiJsonValidator;
 import org.apache.fineract.useradministration.domain.AppUser;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,9 +52,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class LoanDecisionWritePlatformServiceJpaRepositoryImpl implements LoanApplicationDecisionWritePlatformService {
 
     private final PlatformSecurityContext context;
-    private final LoanApplicationTransitionApiJsonValidator loanApplicationTransitionApiJsonValidator;
+    private final LoanDecisionTransitionApiJsonValidator loanDecisionTransitionApiJsonValidator;
     private final LoanRepositoryWrapper loanRepositoryWrapper;
-    private final BusinessEventNotifierService businessEventNotifierService;
+    private final LoanDecisionRepository loanDecisionRepository;
+    private final ConfigurationReadPlatformService configurationReadPlatformService;
+    private final ConfigurationDomainService configurationDomainService;
+    private final CalendarInstanceRepository calendarInstanceRepository;
+    private final LoanScheduleAssembler loanScheduleAssembler;
+    private final LoanUtilService loanUtilService;
+    private final LoanReadPlatformService loanReadPlatformService;
+    private final LoanDecisionAssembler loanDecisionAssembler;
 
     @Transactional
     @Override
@@ -49,20 +69,106 @@ public class LoanDecisionWritePlatformServiceJpaRepositoryImpl implements LoanAp
 
         final AppUser currentUser = getAppUserIfPresent();
 
-        this.loanApplicationTransitionApiJsonValidator.validateApproval(command.json());
+        this.loanDecisionTransitionApiJsonValidator.validateApplicationReview(command.json());
 
         final Loan loan = this.loanRepositoryWrapper.findOneWithNotFoundDetection(loanId, true);
 
-        businessEventNotifierService.notifyPostBusinessEvent(new LoanApprovedBusinessEvent(loan));
+        final GlobalConfigurationPropertyData extendLoanLifeCycleConfig = this.configurationReadPlatformService
+                .retrieveGlobalConfiguration("Add-More-Stages-To-A-Loan-Life-Cycle");
+        final Boolean isExtendLoanLifeCycleConfig = extendLoanLifeCycleConfig.isEnabled();
+
+        if (!isExtendLoanLifeCycleConfig) {
+            throw new GeneralPlatformDomainRuleException("error.msg.Add-More-Stages-To-A-Loan-Life-Cycle.is.not.set",
+                    "Add-More-Stages-To-A-Loan-Life-Cycle settings is not set. So this operation is not permitted");
+        }
+
+        final LoanDecision loanDecision = this.loanDecisionRepository.findLoanDecisionByLoanId(loan);
+        if (loanDecision != null) {
+            throw new GeneralPlatformDomainRuleException("error.msg.loan.account.should.not.exist.in.decision.engine",
+                    "Loan Account found in decision engine. Operation [Review Application] is not allowed");
+        }
+        checkClientOrGroupActive(loan);
+
+        validateLoanDisbursementDataWithMeetingDate(loan);
+        validateLoanTopUp(loan);
+
+        LocalDate loanReviewOnDate = command.localDateValueOfParameterNamed(LoanApiConstants.loanReviewOnDateParameterName);
+        if (loanReviewOnDate.isBefore(loan.getSubmittedOnDate())) {
+            throw new GeneralPlatformDomainRuleException("error.msg.loan.review.application.date.should.be.after.submission.date",
+                    "Loan Review Application date " + loanReviewOnDate + " should be after submission date " + loan.getSubmittedOnDate());
+        }
+
+        if (!loan.status().isSubmittedAndPendingApproval()) {
+            throw new GeneralPlatformDomainRuleException("error.msg.loan.current.status.is.invalid",
+                    "Loan Account current status is invalid. Expected" + loan.status().getCode() + " but found " + loan.status().getCode());
+        }
+        LoanDecision loanDecisionObj = loanDecisionAssembler.assembleFrom(command, loan, currentUser);
+        LoanDecision savedObj = loanDecisionRepository.saveAndFlush(loanDecisionObj);
+
+        Loan loanObj = loan;
+        loanObj.setLoanDecisionState(LoanDecisionState.REVIEW_APPLICATION.getValue());
+        this.loanRepositoryWrapper.saveAndFlush(loanObj);
 
         return new CommandProcessingResultBuilder() //
                 .withCommandId(command.commandId()) //
-                .withEntityId(loan.getId()) //
+                .withEntityId(savedObj.getId()) //
                 .withOfficeId(loan.getOfficeId()) //
                 .withClientId(loan.getClientId()) //
                 .withGroupId(loan.getGroupId()) //
                 .withLoanId(loanId) //
-                .build();
+                .withResourceIdAsString(savedObj.getId().toString()).build();
+    }
+
+    private void validateLoanTopUp(Loan loan) {
+        if (loan.isTopup() && loan.getClientId() != null) {
+            final Long loanIdToClose = loan.getTopupLoanDetails().getLoanIdToClose();
+            final Loan loanToClose = this.loanRepositoryWrapper.findNonClosedLoanThatBelongsToClient(loanIdToClose, loan.getClientId());
+            if (loanToClose == null) {
+                throw new GeneralPlatformDomainRuleException("error.msg.loan.to.be.closed.with.topup.is.not.active",
+                        "Loan to be closed with this topup is not active.");
+            }
+
+            final LocalDate lastUserTransactionOnLoanToClose = loanToClose.getLastUserTransactionDate();
+            if (loan.getDisbursementDate().isBefore(lastUserTransactionOnLoanToClose)) {
+                throw new GeneralPlatformDomainRuleException(
+                        "error.msg.loan.disbursal.date.should.be.after.last.transaction.date.of.loan.to.be.closed",
+                        "Disbursal date of this loan application " + loan.getDisbursementDate()
+                                + " should be after last transaction date of loan to be closed " + lastUserTransactionOnLoanToClose);
+            }
+            BigDecimal loanOutstanding = this.loanReadPlatformService
+                    .retrieveLoanForeclosureTemplate(loanIdToClose, loan.getDisbursementDate()).getAmount();
+            final BigDecimal firstDisbursalAmount = loan.getFirstDisbursalAmount();
+            if (loanOutstanding.compareTo(firstDisbursalAmount) > 0) {
+                throw new GeneralPlatformDomainRuleException("error.msg.loan.amount.less.than.outstanding.of.loan.to.be.closed",
+                        "Topup loan amount should be greater than outstanding amount of loan to be closed.");
+            }
+            BigDecimal netDisbursalAmount = loan.getApprovedPrincipal().subtract(loanOutstanding);
+            loan.adjustNetDisbursalAmount(netDisbursalAmount);
+        }
+    }
+
+    private void validateLoanDisbursementDataWithMeetingDate(Loan loan) {
+        Boolean isSkipRepaymentOnFirstMonth = false;
+        Integer numberOfDays = 0;
+        // validate expected disbursement date against meeting date
+        if (loan.isSyncDisbursementWithMeeting() && (loan.isGroupLoan() || loan.isJLGLoan())) {
+            final CalendarInstance calendarInstance = this.calendarInstanceRepository.findCalendarInstaneByEntityId(loan.getId(),
+                    CalendarEntityType.LOANS.getValue());
+            Calendar calendar = null;
+            if (calendarInstance != null) {
+                calendar = calendarInstance.getCalendar();
+            }
+            // final Calendar calendar = calendarInstance.getCalendar();
+            boolean isSkipRepaymentOnFirstMonthEnabled = this.configurationDomainService.isSkippingMeetingOnFirstDayOfMonthEnabled();
+            if (isSkipRepaymentOnFirstMonthEnabled) {
+                isSkipRepaymentOnFirstMonth = this.loanUtilService.isLoanRepaymentsSyncWithMeeting(loan.group(), calendar);
+                if (isSkipRepaymentOnFirstMonth) {
+                    numberOfDays = configurationDomainService.retreivePeroidInNumberOfDaysForSkipMeetingDate().intValue();
+                }
+            }
+            this.loanScheduleAssembler.validateDisbursementDateWithMeetingDates(loan.getDisbursementDate(), calendar,
+                    isSkipRepaymentOnFirstMonth, numberOfDays);
+        }
     }
 
     private AppUser getAppUserIfPresent() {
@@ -71,6 +177,21 @@ public class LoanDecisionWritePlatformServiceJpaRepositoryImpl implements LoanAp
             user = this.context.getAuthenticatedUserIfPresent();
         }
         return user;
+    }
+
+    private void checkClientOrGroupActive(final Loan loan) {
+        final Client client = loan.client();
+        if (client != null) {
+            if (client.isNotActive()) {
+                throw new ClientNotActiveException(client.getId());
+            }
+        }
+        final Group group = loan.group();
+        if (group != null) {
+            if (group.isNotActive()) {
+                throw new GroupNotActiveException(group.getId());
+            }
+        }
     }
 
 }
