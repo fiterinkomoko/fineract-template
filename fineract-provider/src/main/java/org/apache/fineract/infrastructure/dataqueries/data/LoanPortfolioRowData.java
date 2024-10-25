b47a5f90b615138945c7f977cc4d56375e7aa809
Niceta Nduku <niceta@inkomoko.com>
2024-10-25 14:39:49 +0300

    CGLT-141: Improved GSON serialisation by adding annotations

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/data/LoanPortfolioRowData.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/dataqueries/data/LoanPortfolioRowData.java
@@ -18,7 +18,8 @@
  */
 package org.apache.fineract.infrastructure.dataqueries.data;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -27,210 +28,141 @@ import java.time.LocalDate;
 
 @Data
 public class LoanPortfolioRowData {
-
-        @JsonProperty("Loan Number")
-        private String loanNumber;
-
-        @JsonProperty("Loan Officer")
-        private String loanOfficer;
-
-        @JsonProperty("Client Name")
-        private String clientName;
-
-        @JsonProperty("Client UUID")
-        private String clientUUID;
-
-        @JsonProperty("Loan Product")
-        private String loanProduct;
-
-        @JsonProperty("Purpose")
-        private String purpose;
-
-        @JsonProperty("Department")
-        private String department;
-
-        @JsonProperty("Strata")
-        private String strata;
-
-        @JsonProperty("KIVA Loan ID")
-        private String kivaLoanId;
-
-        @JsonProperty("Funder")
-        private String funder;
-
-        @JsonProperty("Client ID")
-        private String clientId;
-
-        @JsonProperty("Date of Birth")
-        private LocalDate dateOfBirth;
-
-        @JsonProperty("KIVA Client ID")
-        private String kivaClientId;
-
-        @JsonProperty("Cycle")
-        private long cycle;
-
-        @JsonProperty("Cohort")
-        private String cohort;
-
-        @JsonProperty("Gender")
-        private String gender;
-
-        @JsonProperty("Province")
-        private String province;
-
-        @JsonProperty("Sector")
-        private String sector;
-
-        @JsonProperty("Cell")
-        private String cell;
-
-        @JsonProperty("District")
-        private String district;
-
-        @JsonProperty("Nationality")
-        private String nationality;
-
-        @JsonProperty("Telephone")
-        private String telephone;
-
-        @JsonProperty("Mobile No")
-        private String mobileNo;
-
-        @JsonProperty("Submission Date ")
-        private LocalDate submissionDate;
-
-        @JsonProperty("Approval Date")
-        private LocalDate approvalDate;
-
-        @JsonProperty("Disbursement Date")
-        private LocalDate disbursementDate;
-
-        @JsonProperty("Applied Amount")
-        private BigDecimal appliedAmount;
-
-        @JsonProperty("Approved Amount")
-        private BigDecimal approvedAmount;
-
-        @JsonProperty("Disbursed Amount")
-        private BigDecimal disbursedAmount;
-
-        @JsonProperty("Difference")
-        private BigDecimal difference;
-
-        @JsonProperty("Currency Type")
-        private String currencyType;
-
-        @JsonProperty("Re-payment Term")
-        private String repaymentTerm;
-
-        @JsonProperty("Loan Type")
-        private String loanType;
-
-        @JsonProperty("Terms Duration")
-        private short termsDuration;
-
-        @JsonProperty("Actual Payment Amount")
-        private BigDecimal actualPaymentAmount;
-
-        @JsonProperty("Principal Paid")
-        private BigDecimal principalPaid;
-
-        @JsonProperty("Interest Paid")
-        private BigDecimal interestPaid;
-
-        @JsonProperty("Insurance fee Paid")
-        private BigDecimal insuranceFeePaid;
-
-        @JsonProperty("Total Late Fees Paid")
-        private BigDecimal totalLateFeesPaid;
-
-        @JsonProperty("Excess Amount Paid")
-        private BigDecimal excessAmountPaid;
-
-        @JsonProperty("Current Balance")
-        private BigDecimal currentBalance;
-
-        @JsonProperty("Principal Balance")
-        private BigDecimal principalBalance;
-
-        @JsonProperty("Interest Balance")
-        private BigDecimal interestBalance;
-
-        @JsonProperty("Fees Balance")
-        private int feesBalance;
-
-        @JsonProperty("Amount Past Due")
-        private BigDecimal amountPastDue;
-
-        @JsonProperty("Principal Past Due")
-        private BigDecimal principalPastDue;
-
-        @JsonProperty("Interest Past Due")
-        private BigDecimal interestPastDue;
-
-        @JsonProperty("Fees Past Due")
-        private BigDecimal feesPastDue;
-
-        @JsonProperty("Scheduled Principal Amount")
-        private BigDecimal scheduledPrincipalAmount;
-
-        @JsonProperty("Scheduled Interest Amount")
-        private BigDecimal scheduledInterestAmount;
-
-        @JsonProperty("Scheduled Fees Amount")
-        private BigDecimal scheduledFeesAmount;
-
-        @JsonProperty("Scheduled Payment Amount")
-        private BigDecimal scheduledPaymentAmount;
-
-        @JsonProperty("Last Payment Amount")
-        private BigDecimal lastPaymentAmount;
-
-        @JsonProperty("Last Principal Amount")
-        private BigDecimal lastPrincipalAmount;
-
-        @JsonProperty("Last Interest Amount")
-        private BigDecimal lastInterestAmount;
-
-        @JsonProperty("Last Fees Amount")
-        private BigDecimal lastFeesAmount;
-
-        @JsonProperty("Last Late Fees Amount")
-        private BigDecimal lastLateFeesAmount;
-
-        @JsonProperty("Last Excess Amount")
-        private BigDecimal lastExcessAmount;
-
-        @JsonProperty("Days in Arrears")
-        private int daysInArrears;
-
-        @JsonProperty("Installment in Arrears")
-        private long installmentInArrears;
-
-        @JsonProperty("Last Payment Date")
-        private LocalDate lastPaymentDate;
-
-        @JsonProperty("Next Payment Due")
-        private LocalDate nextPaymentDue;
-
-        @JsonProperty("Final Payment Date")
-        private LocalDate finalPaymentDate;
-
-        @JsonProperty("Date Closed")
-        private LocalDate dateClosed;
-
-        @JsonProperty("Loan Status")
-        private String loanStatus;
-
-        @JsonProperty("Business Description")
-        private String businessDescription;
-
-        @JsonProperty("Industry/Sector of Activity")
-        private String industrySectorOfActivity;
-
-        @JsonProperty("Business Sub-Sector")
-        private String businessSubSector;
-
+    @SerializedName("Loan Number")
+    private String loanNumber;
+    @SerializedName("Loan Officer")
+    private String loanOfficer;
+    @SerializedName("Client Name")
+    private String clientName;
+    @SerializedName("Client UUID")
+    private String clientUUID;
+    @SerializedName("Loan Product")
+    private String loanProduct;
+    @SerializedName("Purpose")
+    private String purpose;
+    @SerializedName("Department")
+    private String department;
+    @SerializedName("Strata")
+    private String strata;
+    @SerializedName("KIVA Loan ID")
+    private String kivaLoanId;
+    @SerializedName("Funder")
+    private String funder;
+    @SerializedName("Client ID")
+    private String clientId;
+    @SerializedName("Date of Birth")
+    private LocalDate dateOfBirth;
+    @SerializedName("KIVA Client ID")
+    private String kivaClientId;
+    @SerializedName("Cycle")
+    private long cycle;
+    @SerializedName("Cohort")
+    private String cohort;
+    @SerializedName("Gender")
+    private String gender;
+    @SerializedName("Province")
+    private String province;
+    @SerializedName("Sector")
+    private String sector;
+    @SerializedName("Cell")
+    private String cell;
+    @SerializedName("District")
+    private String district;
+    @SerializedName("Nationality")
+    private String nationality;
+    @SerializedName("Telephone")
+    private String telephone;
+    @SerializedName("Mobile No")
+    private String mobileNo;
+    @SerializedName("Submission Date ")
+    private LocalDate submissionDate;
+    @SerializedName("Approval Date")
+    private LocalDate approvalDate;
+    @SerializedName("Disbursement Date")
+    private LocalDate disbursementDate;
+    @SerializedName("Applied Amount")
+    private BigDecimal appliedAmount;
+    @SerializedName("Approved Amount")
+    private BigDecimal approvedAmount;
+    @SerializedName("Disbursed Amount")
+    private BigDecimal disbursedAmount;
+    @SerializedName("Difference")
+    private BigDecimal difference;
+    @SerializedName("Currency Type")
+    private String currencyType;
+    @SerializedName("Re-payment Term")
+    private String repaymentTerm;
+    @SerializedName("Loan Type")
+    private String loanType;
+    @SerializedName("Terms Duration")
+    private short termsDuration;
+    @SerializedName("Actual Payment Amount")
+    private BigDecimal actualPaymentAmount;
+    @SerializedName("Principal Paid")
+    private BigDecimal principalPaid;
+    @SerializedName("Interest Paid")
+    private BigDecimal interestPaid;
+    @SerializedName("Insurance fee Paid")
+    private BigDecimal insuranceFeePaid;
+    @SerializedName("Total Late Fees Paid")
+    private BigDecimal totalLateFeesPaid;
+    @SerializedName("Excess Amount Paid")
+    private BigDecimal excessAmountPaid;
+    @SerializedName("Current Balance")
+    private BigDecimal currentBalance;
+    @SerializedName("Principal Balance")
+    private BigDecimal principalBalance;
+    @SerializedName("Interest Balance")
+    private BigDecimal interestBalance;
+    @SerializedName("Fees Balance")
+    private int feesBalance;
+    @SerializedName("Amount Past Due")
+    private BigDecimal amountPastDue;
+    @SerializedName("Principal Past Due")
+    private BigDecimal principalPastDue;
+    @SerializedName("Interest Past Due")
+    private BigDecimal interestPastDue;
+    @SerializedName("Fees Past Due")
+    private BigDecimal feesPastDue;
+    @SerializedName("Scheduled Principal Amount")
+    private BigDecimal scheduledPrincipalAmount;
+    @SerializedName("Scheduled Interest Amount")
+    private BigDecimal scheduledInterestAmount;
+    @SerializedName("Scheduled Fees Amount")
+    private BigDecimal scheduledFeesAmount;
+    @SerializedName("Scheduled Payment Amount")
+    private BigDecimal scheduledPaymentAmount;
+    @SerializedName("Last Payment Amount")
+    private BigDecimal lastPaymentAmount;
+    @SerializedName("Last Principal Amount")
+    private BigDecimal lastPrincipalAmount;
+    @SerializedName("Last Interest Amount")
+    private BigDecimal lastInterestAmount;
+    @SerializedName("Last Fees Amount")
+    private BigDecimal lastFeesAmount;
+    @SerializedName("Last Late Fees Amount")
+    private BigDecimal lastLateFeesAmount;
+    @SerializedName("Last Excess Amount")
+    private BigDecimal lastExcessAmount;
+    @SerializedName("Days in Arrears")
+    private int daysInArrears;
+    @SerializedName("Installment in Arrears")
+    private long installmentInArrears;
+    @SerializedName("Last Payment Date")
+    private LocalDate lastPaymentDate;
+    @SerializedName("Next Payment Due")
+    private LocalDate nextPaymentDue;
+    @SerializedName("Final Payment Date")
+    private LocalDate finalPaymentDate;
+    @SerializedName("Date Closed")
+    private LocalDate dateClosed;
+    @SerializedName("Loan Status")
+    private String loanStatus;
+    @SerializedName("Business Description")
+    private String businessDescription;
+    @SerializedName("Industry/Sector of Activity")
+    private String industrySectorOfActivity;
+    @SerializedName("Business Sub-Sector")
+    private String businessSubSector;
 
 }
