2525291e972fe8284234ad0d7957e477abfa1095
Niceta Nduku <niceta@inkomoko.com>
2025-04-29 13:58:53 +0300

    CGLT-190: removed unused imports

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/KivaLoanServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/loanaccount/service/KivaLoanServiceImpl.java
@@ -35,7 +35,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
-import liquibase.pro.packaged.S;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
